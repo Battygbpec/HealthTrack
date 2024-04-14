691944b1bce895f34a5537f33d9995591bd75786
Vivek Singh <vivek.singh08@nagarro.com>
2024-04-14 23:24:47 +0530

    adding persistence data

---
+++ src/main/java/com/bits/wilp/persistence/entity/Relationships.java
@@ -0,0 +1,14 @@
+package com.bits.wilp.persistence.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Relationships {
+    @Id
+    private Long id;
+}
